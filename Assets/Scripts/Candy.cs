769b7f0eca9babffbffd5357c60c4bb4e252d350
webSeungKyu <abcsksk1@gmail.com>
2024-04-28 21:43:46 +0900

    위치 변경 테스트

--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -5,26 +5,25 @@ using UnityEngine;
 public class Candy : MonoBehaviour
 {
     private Vector3 offset;
+    public Vector3 firstPos;
 
     void OnMouseDown()
     {
-        Debug.Log("OnMouseDown");
         // 마우스 클릭 시 게임 오브젝트의 현재 위치와 마우스 포인터의 위치 간의 차이를 계산하여 저장
         offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void OnMouseDrag()
     {
-        Debug.Log("OnMouseDrag");
         // 마우스 드래그 시 새로운 위치 계산 후 적용
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
         transform.position = newPosition;
     }
 
-    // Start is called before the first frame update
+    
     void Start()
     {
-
+        firstPos = transform.position;
     }
 
     // Update is called once per frame
@@ -33,7 +32,13 @@ public class Candy : MonoBehaviour
 
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"{gameObject.name}는 {collision.name}과 충돌하여 위치 변경");
+        collision.transform.position = firstPos;
+        gameObject.transform.position = collision.GetComponent<Candy>().firstPos;
 
-    
+    }
+
 
 }
